08770713e42264120abe88fc45dc552efe9f6203
dave0927 <dawittadesse927@gmail.com>
2023-04-24 04:37:50 -0700

    minor fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,21 +10,21 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    size_t j;
-    int temp;
+	size_t i;
+	size_t j;
+	int temp;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < (size - i - 1); j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
