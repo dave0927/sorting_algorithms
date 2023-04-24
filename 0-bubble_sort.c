68c15ca6c2c4b739bc119249b1a372fc5822b17f
Dawit Tadesse <dawittadesse927@gmail.com>
2023-04-24 14:14:22 +0300

    Revert "Task 0: minor fix"
    This reverts commit 54e951014c463f5e07b126af00aa3a4b53b293b8.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 #include <stdio.h>
-#include <stdbool.h>
 
 /**
  * bubble_sort - Use bubble sort method to sort an array
@@ -8,31 +7,24 @@
  * @array: The array to be sorted
  * @size: The size of the array
  */
+
 void bubble_sort(int *array, size_t size)
 {
     size_t i;
     size_t j;
     int temp;
-    bool swapped;
 
-    for (i = 0; i < size - 1; i++)
+    for (i = 0; i < size; i++)
     {
-        swapped = false;
-
-        for (j = 0; j < size - i - 1; j++)
+        for (j = 0; j < (size - i - 1); j++)
         {
             if (array[j] > array[j + 1])
             {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-                swapped = true;
+                print_array(array, size);
             }
-        }
-
-        if (!swapped)
-        {
-            break;
         }
     }
 }
