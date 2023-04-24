d82c321708d0093ca91b20fb9298f7c8b5b5465e
dave0927 <dawittadesse927@gmail.com>
2023-04-24 04:51:47 -0700

    minor fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,11 +7,11 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -24,22 +24,23 @@ void swap_ints(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-    int *min;
-    size_t i, j;
+	int *min;
+	size_t i, j;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = array + i;
-        for (j = i + 1; j < size; j++)
-            min = (array[j] < *min) ? (array + j) : min;
-
-        if ((array + i) != min)
-        {
-            swap_ints(array + i, min);
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+		{
+			min = (array[j] < *min) ? (array + j) : min;
+			if ((array + i) != min)
+			{
+				swap_ints(array + i, min);
+				print_array(array, size);
+			}
+		}
+	}
 }
