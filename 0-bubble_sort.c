54e951014c463f5e07b126af00aa3a4b53b293b8
Dawit Tadesse <dawittadesse927@gmail.com>
2023-04-24 14:11:18 +0300

    Task 0: minor fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 /**
  * bubble_sort - Use bubble sort method to sort an array
@@ -7,24 +8,31 @@
  * @array: The array to be sorted
  * @size: The size of the array
  */
-
 void bubble_sort(int *array, size_t size)
 {
     size_t i;
     size_t j;
     int temp;
+    bool swapped;
 
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size - 1; i++)
     {
-        for (j = 0; j < (size - i - 1); j++)
+        swapped = false;
+
+        for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
             {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-                print_array(array, size);
+                swapped = true;
             }
+        }
+
+        if (!swapped)
+        {
+            break;
         }
     }
 }
